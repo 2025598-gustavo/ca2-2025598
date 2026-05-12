f60798c5c1d396de2b540094a85292a0ec99f0eb
2025598-gustavo <2025598@student.cct.ie>
2026-05-12 11:45:03 +0100

    feat: implementation getEmployeeNames in EmployeeService

--- CA_2/src/main/java/com/bank/ca_2/services/EmployeeService.java
+++ CA_2/src/main/java/com/bank/ca_2/services/EmployeeService.java
@@ -19,4 +19,15 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
         return employees;
     }
+    
+    public List<String> getEmployeeNames() {
+
+    List<String> employeeNames = new ArrayList<>();
+
+    for (Employee employee : employees) {
+        employeeNames.add(employee.getFullName());
+    }
+
+    return employeeNames;
+}
 }
